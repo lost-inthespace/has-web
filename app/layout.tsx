dd1cd843b29c67aa874b4264e064887d18b134f0
Mohammad Alzmzmi <mhd.yosof@gmail.com>
2025-02-16 02:46:57 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/contexts/ThemeContext"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
